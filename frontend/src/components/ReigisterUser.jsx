4a2cc5d359c2f9e014650e834840e1f7d3756e32
Nikhil <nikhildmagar@gmail.com>
2024-07-18 22:31:24 +0530

    minor fixes

--- frontend/src/components/ReigisterUser.jsx
+++ frontend/src/components/ReigisterUser.jsx
@@ -51,6 +51,10 @@ export default function RegisterUser() {
             toast.dismiss()
             toast.success('Signed Up successfully!')
         }
+        else if(response.status === 400){
+            toast.dismiss()
+            toast.success('User already exists')
+        }
         else {
             toast.dismiss()
             toast.error('Failed to Sign Up')
@@ -115,7 +119,7 @@ export default function RegisterUser() {
 
                             <div>
                                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                                <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nikhil Magar" required="" value={name} onChange={(e) => setName(e.target.value)} />
+                                <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" required="" value={name} onChange={(e) => setName(e.target.value)} />
                             </div>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
