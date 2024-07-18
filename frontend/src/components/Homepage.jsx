2652ed9ea13c16d680978aa7d91d0c67f473db85
Nikhil <nikhildmagar@gmail.com>
2024-07-18 22:03:57 +0530

    fixed deployment bugs/warns

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -118,7 +118,7 @@ export default function Homepage() {
     useEffect(() => {
         fetchUser()
         setIsMounted(true);
-    }, [])
+    },)
 
 
     return (
@@ -177,7 +177,7 @@ export default function Homepage() {
                                     className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                                     type="button"
                                 >
-                                    Yes, I'm sure
+                                    Yes, I&apos;m sure
                                 </button>
                                 <button
                                     onClick={closeModal}
